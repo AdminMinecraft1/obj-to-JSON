46c0e4cc679683692f9f4da02bd77376a999a2ed
Sergey Filyuk <sfi@ciklum.com>
2014-10-06 17:45:26 +0300

    add Mesh object.

--- objToJSON.js
+++ objToJSON.js
@@ -1,70 +1,94 @@
 var fs = require('fs');
 var src = './../../../client/models/Vehicles/1/ARC170.obj';
 
-var objToJSON = function(src){
+var Mesh = function Mesh() {
+    this.name = '';
+    this.src = '';
+    this.v = [];
+    this.vn = [];
+    this.vt = [];
+    this.f = [];
+    this.vertices = [];
 
-	var model = {
-		v:[],
-		vt:[],
-		vn:[],
-		f:[],
-                vertices:[]
-	};
+    return this
+}
 
-	var format = function(array,reg){
+Mesh.prototype.get = function(a) {
+    if (!a) {
+        throw console.error('None such property')
+    }
+    return this[a];
+}
 
-		array.map(function(v0,i0){
-			array[i0] = v0.match(reg);
-		});
+Mesh.prototype.set = function(a, b) {
+    if (!a) {
+        throw console.error('None such property')
+    }
+    this[a] = b;
+}
 
-		return array;
-	}
+var objToJSON = function(src) {
 
-	var add = function(string,name,model){
-		var reg = new RegExp(name+'\\s.*\\r\\n','g');
-		var array = string.match(reg);
-		if(array !== null)
-		{		
-			model[name] = model[name]
-								.concat(format(
-										array,
-										new RegExp('[^'+name+'\\s*]+[\\w\/][^\\s]*','g')
-									));
-		}
-	}
+    var path,
+            name,
+            type,
+            mesh = new Mesh(),
+            stream = fs.createReadStream(src),
+            format = function(array, reg) {
 
-	var stream = fs.createReadStream(src);
+                array.map(function(v0, i0) {
+                    array[i0] = v0.match(reg);
+                });
 
-
-	stream.on('data',function(line){
-		line = line.toString();
-
-		add(line,'v',model);
-		add(line,'vt',model);
-		add(line,'vn',model);
-		add(line,'f',model);
-
-	
-	})
-        
-        model.v.map(function(v0,i0){
-            v0.map(function(v1,i1){
-                if(v1.length){
-                    model.vertices.push(parseFloat(v1));
+                return array;
+            },
+            add = function(string, name, model) {
+                var reg = new RegExp(name + '\\s.*\\r\\n', 'g');
+                var array = string.match(reg);
+                if (array !== null)
+                {
+                    model[name] = model[name]
+                            .concat(format(
+                                    array,
+                                    new RegExp('[^' + name + '\\s*]+[\\w\/][^\\s]*', 'g')
+                                    ));
                 }
-            })
-        });
-        
-        var path = src.match(new RegExp('\/[^\/]*$','g'))[0];
-        var name =  path.replace('/','').match(new RegExp('[^\.]\\w*'))[0];
-        var type = path.replace('/','').match(new RegExp('[^\.]\\w*$','g'))[0];
-        
+            };
 
-	stream.on('end',function(){
-		fs.writeFile(src.replace(path,'')+'/'+name+'.json',JSON.stringify(model),function(){
 
-		})
-	})
+
+    stream.on('data', function(line) {
+        line = line.toString();
+
+        add(line, 'v', mesh);
+        add(line, 'vt', mesh);
+        add(line, 'vn', mesh);
+        add(line, 'f', mesh);
+
+
+    })
+
+    mesh.get('v').map(function(v0, i0) {
+        v0.map(function(v1, i1) {
+            if (v1.length) {
+                mesh.get('vertices').push(parseFloat(v1));
+            }
+        })
+    });
+
+    path = src.match(new RegExp('\/[^\/]*$', 'g'))[0];
+    name = path.replace('/', '').match(new RegExp('[^\.]\\w*'))[0];
+    type = path.replace('/', '').match(new RegExp('[^\.]\\w*$', 'g'))[0];
+
+    mesh.set('name', name);
+    mesh.set('src', src.replace(path, '') + '/' + name + '.json');
+
+
+    stream.on('end', function() {
+        fs.writeFile(mesh.get('src'), JSON.stringify(mesh), function() {
+
+        })
+    })
 
 }
 
